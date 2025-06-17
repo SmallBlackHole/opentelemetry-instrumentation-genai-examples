4400dcff2f6b002cdc824620454d62de88196b25
Kuojian Lu <kuojianlu@gmail.com>
2025-06-17 11:16:13 +0800

    add requirements for anthropic

--- python/anthropic/main.py
+++ python/anthropic/main.py
@@ -1,13 +1,25 @@
-import anthropic
-import logfire
 import os
+import anthropic
 
-os.environ["OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"] = "http://localhost:4318/v1/traces"
-logfire.configure(
-    service_name="test_anthropic_logfire",
-    send_to_logfire=False,
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+resource = Resource(attributes={
+    "service.name": "opentelemetry-instrumentation-anthropic-traceloop"
+})
+provider = TracerProvider(resource=resource)
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://localhost:4318/v1/traces",
 )
-logfire.instrument_anthropic()
+processor = BatchSpanProcessor(otlp_exporter)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+from opentelemetry.instrumentation.anthropic import AnthropicInstrumentor
+AnthropicInstrumentor().instrument()
 
 client = anthropic.Anthropic(
     api_key=os.environ["ANTHROPIC_API_KEY"],
