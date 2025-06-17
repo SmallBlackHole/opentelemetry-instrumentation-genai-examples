4505eb229143aa02a6a73a1d6730b17e6d4116a2
Kuojian Lu <kuojianlu@gmail.com>
2025-06-17 10:43:43 +0800

    add requirements for langchain

--- python/langchain/main.py
+++ python/langchain/main.py
@@ -1,14 +1,17 @@
 import os
+
+### Set up for OpenTelemetry tracing ###
 os.environ["LANGSMITH_OTEL_ENABLED"] = "true"
 os.environ["LANGSMITH_TRACING"] = "true"
 os.environ["OTEL_EXPORTER_OTLP_ENDPOINT"] = "http://localhost:4318"
+### Set up for OpenTelemetry tracing ###
 
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 
 # Create a chain
 prompt = ChatPromptTemplate.from_template("Tell me a joke about {topic}")
-model = ChatOpenAI()
+model = ChatOpenAI(openai_api_key=os.environ["OPENAI_API_KEY"], model_name="gpt-4o")
 chain = prompt | model
 
 # Run the chain
